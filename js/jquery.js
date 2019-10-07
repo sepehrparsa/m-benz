7ecdb877042e87812da30217cce2a29da2b33b49
sepehrparsa <56133318+sepehrparsa@users.noreply.github.com>
2019-10-07 20:59:09 +0330

    Add files via upload

---
+++ js/jquery.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    $("#enterbtn").click(function() {
+        $("#hidden-1").fadeOut("slow");
+        $("#carse").fadeIn("slow");
+
+
+
+
+    })
+});
+
+$(document).ready(function() {
+    $("#carse").hover(function() {
+        $("#jumb").fadeIn("slow");
+    })
+});
+
+
+
+$(document).ready(function() {
+    $("#enterbtn").click(function() {
+        $("#hidden-1").fadeOut("slow");
+        $("#cardc").fadeIn("slow");
+    })
+});
+
+$(document).ready(function() {
+    $("#flip").click(function() {
+        $("#panel").slideToggle("slow");
+    });
+});
+
+
+$(document).ready(function() {
+    $("#flip1").click(function() {
+        $("#panel1").slideToggle("slow");
+    });
+});
